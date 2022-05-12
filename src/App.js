c96b79841dd28c0612caa452c48a7e83d0eace52
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 12:56:20 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./style.css";
   import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faMinus } from '@fortawesome/free-solid-svg-icons' 
+import Header from './Header'
 export default function App() {
   return (
     <div>
+      <Header />
       <FontAwesomeIcon icon={faMinus}  />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
