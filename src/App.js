0f37ef76692c0b47bfdbf4e10cf8894a3d74135d
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 10:57:22 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
-import FontAwesomeIcon from "@fortawesome"
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faMinus } from '@fortawesome/free-solid-svg-icons' 
 export default function App() {
   return (
     <div>
-      <FontAwesomeIcon icon="fa-regular fa-plus" />
+      <FontAwesomeIcon icon={faMinus}  />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
