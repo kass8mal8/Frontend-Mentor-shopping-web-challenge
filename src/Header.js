562d3a6b3fa442a240881b11df6660750a583177
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 13:08:35 +0300

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faMenu } from '@fortawesome/free-solid-svg-icons' 
 
 const Header =()=>{
   return(
     <div>
-      
+        <FontAwesomeIcon icon={faMenu}></FontAwesomeIcon>
     </div>
   )
 }
