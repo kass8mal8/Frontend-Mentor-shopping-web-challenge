4b8f330ac10fcdd16df4cb494d574439d53ac6c8
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 22:39:54 +0300

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,14 @@ import "./style.css"
 
 const Header =()=>{
   const avatar="https://aakashhelloworld.github.io/css2/images/avatar-shanai.png"
+  const listItems=["collections", "men", "women", "about","contacts"]
+
+  listItems.map(list=>(<li>{list} </li>))
   return(
     <div>
         <div className="h-div">
         <FontAwesomeIcon icon={faBars} className="h-div--bars"></FontAwesomeIcon><h3>sneakers</h3>
+        <ul>{listItems}</ul>
         <FontAwesomeIcon icon={faCartShopping} className="h-div--cart"></FontAwesomeIcon>
         <img src={avatar} width="50px" alt="avatar"/>
         </div>
