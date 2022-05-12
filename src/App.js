56bd6bf715f550a210e297c7213b5d4bdb584147
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 20:19:52 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./style.css";
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faBars, faCartShopping} from '@fortawesome/free-solid-svg-icons' 
+
 import Header from './Header'
 export default function App() {
   return (
     <div>
       <Header />
-      <FontAwesomeIcon icon={faBars}  />
-      <FontAwesomeIcon icon={faCartShopping} />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faMenu } from '@fortawesome/free-solid-svg-icons' 
+  import { faBars,faCartShopping } from '@fortawesome/free-solid-svg-icons' 
 
 const Header =()=>{
   return(
     <div>
-        <FontAwesomeIcon icon={faMenu}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
     </div>
   )
 }
