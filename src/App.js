9c60161ffbf2245c70f662c39bdc6d294f263f19
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 10:49:40 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./style.css";
-
+import FontAwesomeIcon from "@fortawesome"
 export default function App() {
   return (
     <div>
+      <FontAwesomeIcon icon="fa-regular fa-plus" />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
