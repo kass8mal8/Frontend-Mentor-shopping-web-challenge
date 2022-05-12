5c1a64475f05588bcdc82c399eb97f22a2ad057a
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 21:56:36 +0300

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./style.css"
 
 const Header =()=>{
+  const avatar="https://aakashhelloworld.github.io/css2/images/avatar-shanai.png"
   return(
     <div>
         <div className="h-div">
         <FontAwesomeIcon icon={faBars} className="h-div--bars"></FontAwesomeIcon><h3>sneakers</h3>
         <FontAwesomeIcon icon={faCartShopping} className="h-div--cart"></FontAwesomeIcon>
+        <img src={avatar} width="50px" alt="avatar"/>
         </div>
     </div>
   )
