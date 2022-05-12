f3b22fd00988c0c81afa0a8739054f22558bb63a
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 23:23:56 +0300

    2 files modified

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,8 @@ const Header =()=>{
     <div>
         <div className="h-div">
         <FontAwesomeIcon icon={faBars} className="h-div--bars"></FontAwesomeIcon><h3>sneakers</h3>
-        <ul className="top-nav">
+        <div className="top-nav">
+        <ul className="top-nav--wrapper">
           <li>collections</li>
           <li>men</li>
           <li>women</li>
@@ -18,6 +19,7 @@ const Header =()=>{
           <li>contacts</li>
 
         </ul>
+        </div>
         <FontAwesomeIcon icon={faCartShopping} className="h-div--cart"></FontAwesomeIcon>
         <img src={avatar} width="50px" alt="avatar"/>
         </div>
