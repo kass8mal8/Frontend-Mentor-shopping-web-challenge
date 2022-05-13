4bd7b6dae815a76c52fc9c2dd407caa919ad7740
kass8mal8 <kassimaly21@gmail.com>
2022-05-13 14:11:20 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./style.css";
 
 import Header from './Header'
+import Main from './Main'
 export default function App() {
   return (
-    <div>
+    <div>   
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main=()=>{
+  const image="https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg"
+  return (
+    <div className="m-div">
+      <div className="m-div--image">
+         <img src={image} />
+      </div>
+    </div>
+  )
+}
+export default Main
