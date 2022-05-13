93066dde377de29d697a2c264c05661e6b4b5f09
kass8mal8 <kassimaly21@gmail.com>
2022-05-13 07:46:47 +0300

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -6,12 +6,15 @@ import "./style.css"
 const Header =()=>{
   const avatar="https://aakashhelloworld.github.io/css2/images/avatar-shanai.png"
 
+  const handleToggle=()=>{
+    document.getElementById("toggle").classList.toggle("active")
+  }
   return(
     <div>
         <div className="h-div">
-        <FontAwesomeIcon icon={faBars} className="h-div--bars"></FontAwesomeIcon><h3>sneakers</h3>
-        <div className="top-nav">
-          <FontAwesomeIcon icon={faClose} className="top-nav--close"></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faBars} className="h-div--bars" onClick={handleToggle}></FontAwesomeIcon><h3>sneakers</h3>
+        <div className="top-nav" id="toggle">
+          <FontAwesomeIcon icon={faClose} className="top-nav--close" onClick={handleToggle}></FontAwesomeIcon>
         <ul className="top-nav--wrapper">
           <li>collections</li>
           <li>men</li>
